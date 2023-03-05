8c776bbfc2091e5e64382cd166192ffbdccf12a6
flothesof <florian.s.lebourdais@gmail.com>
2023-03-05 18:07:01 +0100

    add mbp script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # naspberry_pi
 A repository for my Raspberry Pi NAS project.
+
+## Scripts
+The repo contains various rsync scripts used in the NAS setup.
+
+- `backup_mbp.sh`: the rsync used to backup my Macbook Pro
+
---
+++ backup_mbp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# backup script for macbook pro
+# meaning of rsync switches
+# -v verbose
+# -a archive mode
+# -u update mode (copy only things that are new)
+# -h human readable
+# -P (–partial —progress)
+# -i output a change-summary for all updates
+rsync -avuhPi --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" /Volumes/Macintosh\ HD/Users/kappamaki/Documents/ /Users/kappamaki/shares/Florian/backup_macbookpro/
+
+
+# Things to backup too:
+# Pictures
+# Movies
+# Downloads
+# Desktop
+# Music
