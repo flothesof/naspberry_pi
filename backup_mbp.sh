d59f33878bab7dc671d64b6a425d08a31e65ca15
flothesof <florian.s.lebourdais@gmail.com>
2023-03-06 20:45:58 +0100

    add backup script for primary drive

--- backup_mbp.sh
+++ backup_mbp.sh
@@ -7,7 +7,7 @@
 # -h human readable
 # -P (–partial —progress)
 # -i output a change-summary for all updates
-rsync -avuhPi --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" /Volumes/Macintosh\ HD/Users/kappamaki/Documents/ /Users/kappamaki/shares/Florian/backup_macbookpro/ | tee backup_mbp.log
+rsync -avuhP --exclude ".DS_Store" -- "__pycache__/" --exclude ".ipynb_checkpoints/" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" /Volumes/Macintosh\ HD/Users/kappamaki/Documents/ /Users/kappamaki/shares/Florian/backup_macbookpro/ | tee backup_mbp.log
 
 
 # Things to backup too:
