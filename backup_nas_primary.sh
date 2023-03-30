e08d76e483483da02aa82f6ed314e3f37bc23ec3
flothesof <florian.s.lebourdais@gmail.com>
2023-03-30 22:19:53 +0200

    add --delete flag

--- backup_nas_primary.sh
+++ backup_nas_primary.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # this file backups the primary nas to the secondary nas disk
 # this file should be run regularly using cron
-rsync -avuhP /media/pi/piNAS/ /media/pi/TOSHIBA\ EXT/ | tee backup_nas_primary.log
+rsync -avuhP --delete /media/pi/piNAS/ /media/pi/TOSHIBA\ EXT/
